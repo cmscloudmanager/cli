e689881276615fbfdc29dc4d828399f22ff74c3f
Patrick Michl <patrick.michl@hetzner.com>
2025-03-15 15:41:16 +0100

    add draft for server creation via hcloud api

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A Nix-flake-based Python development environment";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          venvDir = ".venv";
+          packages = with pkgs; [ python313 ] ++
+            (with pkgs.python313Packages; [
+              pip
+              venvShellHook
+            ]);
+        };
+      });
+    };
+}
