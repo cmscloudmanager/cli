668a94313067c83a2121993317f90dabf92d3405
Matthias ÃUebler <tx32cv@gmail.com>
2025-03-15 18:38:43 +0100

    refactor prepare server and run ansible

--- prepare_server_and_run_ansible.py
+++ prepare_server_and_run_ansible.py
@@ -15,139 +15,152 @@ def print_step(step):
   print("\033[90m{}\033[0m".format(step))
 
 def print_error(message):
-  print("\033[91m{}\033[0m".format(step), file=sys.stderr)
+  print("\033[91m{}\033[0m".format(message), file=sys.stderr)
 
-def wait_for_ssh(host):
-  waiting_since = time.time_ns()
+class Server:
+  def __init__(self, host):
+    self.host = host
 
-  while time.time_ns() < waiting_since + (SSH_MAX_WAIT_TIME_SECONDS * 10 ** 6):
-    res = subprocess.run(["/usr/bin/env", "ssh-keyscan", "-H", "-T", str(SSH_WAIT_TIMEOUT_SECONDS), host], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+  def wait_for_ssh(self):
+    waiting_since = time.time_ns()
 
-    if res.returncode == 0:
-      return
+    while time.time_ns() < waiting_since + (SSH_MAX_WAIT_TIME_SECONDS * 10 ** 6):
+      res = subprocess.run(["/usr/bin/env", "ssh-keyscan", "-H", "-T", str(SSH_WAIT_TIMEOUT_SECONDS), self.host], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
-    time.sleep(WAIT_FOR_SSH_SLEEP_TIME_SECONDS)
+      if res.returncode == 0:
+        return
 
-  print_error("wait for ssh failed")
+      time.sleep(WAIT_FOR_SSH_SLEEP_TIME_SECONDS)
 
-  exit(1)
+    print_error("wait for ssh failed")
 
-def ssh_keyscan(host):
-  res = subprocess.run(["/usr/bin/env", "ssh-keyscan", "-H", "-T", str(SSH_WAIT_TIMEOUT_SECONDS), host], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    exit(1)
 
-  if res.returncode != 0:
-    print_error("ssh-keycan {} failed: {}".format(host, res.stderr.decode("utf-8")))
-
-    exit(2)
-
-  return res.stdout.decode("utf-8")
-
-def prepare_known_hosts_file(host):
-  known_hosts = ssh_keyscan(host)
-
-  with open(KNOWN_HOSTS_FILE, "w") as file:
-    file.write(known_hosts)
-
-def ssh_exec(host, code, pipe = True):
-  if pipe:
-    stdout = subprocess.PIPE
-    stderr = subprocess.PIPE
-  else:
-    stdout = None
-    stderr = None
-
-  res = subprocess.run(["/usr/bin/env", "ssh", "-o", "PasswordAuthentication=no", "-o", "UserKnownHostsFile={}".format(KNOWN_HOSTS_FILE), "-l", "root", host, "/usr/bin/env", "bash"], input=code.encode("utf-8"), stdout=stdout, stderr=stderr)
-
-  return res
-
-def update_server(host):
-  for code in ["apt-get update", "apt-get -y upgrade", "apt-get -y install ansible"]:
-
-    res = ssh_exec(host, code, False)
+  def ssh_keyscan(self):
+    res = subprocess.run(["/usr/bin/env", "ssh-keyscan", "-H", "-T", str(SSH_WAIT_TIMEOUT_SECONDS), self.host], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     if res.returncode != 0:
-      print_error("fatal error: {} failed".format(code))
+      print_error("ssh-keycan {} failed: {}".format(self.host, res.stderr.decode("utf-8")))
+
+      exit(2)
+
+    return res.stdout.decode("utf-8")
+
+  def prepare_known_hosts_file(self):
+    known_hosts = self.ssh_keyscan()
+
+    with open(KNOWN_HOSTS_FILE, "w") as file:
+      file.write(known_hosts)
+
+  def ssh_exec(self, code, pipe = True):
+    if pipe:
+      stdout = subprocess.PIPE
+      stderr = subprocess.PIPE
+    else:
+      stdout = None
+      stderr = None
+
+    res = subprocess.run([
+      "/usr/bin/env",
+      "ssh",
+      "-o", "PasswordAuthentication=no",
+      "-o", "UserKnownHostsFile={}".format(KNOWN_HOSTS_FILE),
+      "-l", "root",
+      self.host,
+      "/usr/bin/env", "bash"], input=code.encode("utf-8"), stdout=stdout, stderr=stderr)
+
+    return res
+
+  def update_server(self):
+    for code in ["apt-get update", "apt-get -y upgrade", "apt-get -y install ansible"]:
+
+      res = self.ssh_exec(code, False)
+
+      if res.returncode != 0:
+        print_error("fatal error: {} failed".format(code))
+
+        exit(4)
+
+  def install_ansible(self):
+    for code in ["apt-get -y install ansible"]:
+
+      res = self.ssh_exec(code, False)
+
+      if res.returncode != 0:
+        print_error("fatal error: {} failed".format(code))
+
+        exit(5)
+
+  def upload_ansible_code(self):
+    # remove remains of previous runs
+    self.ssh_exec("test -e ansible && rm -r ansible")
+
+    res = subprocess.run([
+      "/usr/bin/env",
+      "scp",
+      "-o", "PasswordAuthentication=no",
+      "-o", "User=root",
+      "-o", "UserKnownHostsFile={}".format(KNOWN_HOSTS_FILE),
+      "-r",
+      os.path.join(os.path.dirname(__file__), "ansible"),
+      "{}:./ansible".format(self.host)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    if res.returncode != 0:
+      print_error("fatal error: scp ansible failed: {}".format(code ,res.stderr.decode("utf-8")))
+
+      exit(6)
+
+  def install_ansible_requirements(self):
+    res = self.ssh_exec("cd ansible && ansible-galaxy install -r requirements.yml -p roles -f", False)
+
+    if res.returncode != 0:
+      print_error("fatal error: installing ansible requirements failed")
+
+      exit(7)
+
+  def run_ansible(self):
+    res = self.ssh_exec("cd ansible && ansible-playbook --connection=local --inventory 127.0.0.1, playbook.yml -e letsencrypt_email=something@example.com", False)
+
+    if res.returncode != 0:
+      print_error("fatal error: running ansbible failed")
+
+  def prepare_server_and_run_ansible(self):
+    print("\033[95msetting up server {} for ansible\033[0m".format(self.host))
+
+    print_step("waiting for server to respond to ssh")
+
+    self.wait_for_ssh()
+
+    self.prepare_known_hosts_file()
+
+    res = self.ssh_exec("true")
+
+    if res.returncode != 0:
+      print_error("fatal error: can not connect server via ssh: {}".format(res.stderr.decode("utf-8")))
 
       exit(3)
 
-def install_ansible(host):
-  for code in ["apt-get -y install ansible"]:
+    print_step("updating server")
 
-    res = ssh_exec(host, code, False)
+    self.update_server()
 
-    if res.returncode != 0:
-      print_error("fatal error: {} failed".format(code))
+    print_step("installing ansible")
 
-      exit(4)
+    self.install_ansible()
 
-def upload_ansible_code(host):
-  # remove remains of previous runs
-  ssh_exec(host, "test -e ansible && rm -r ansible")
+    print_step("uploading ansible code")
 
-  res = subprocess.run([
-    "/usr/bin/env",
-    "scp",
-    "-o", "PasswordAuthentication=no",
-    "-o", "User=root",
-    "-o", "UserKnownHostsFile={}".format(KNOWN_HOSTS_FILE),
-    "-r",
-    os.path.join(os.path.dirname(__file__), "ansible"),
-    "{}:./ansible".format(host)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    self.upload_ansible_code()
 
-  if res.returncode != 0:
-    print_error("fatal error: scp ansible failed: {}".format(code ,res.stderr.decode("utf-8")))
+    print_step("installing ansible requirements")
 
-    exit(5)
+    self.install_ansible_requirements()
 
-def install_ansible_requirements(host):
-  res = ssh_exec(host, "cd ansible && ansible-galaxy install -r requirements.yml -p roles -f", False)
+    print_step("running ansible")
 
-  if res.returncode != 0:
-    print_error("fatal error: installing ansible requirements failed")
-
-    exit(6)
-
-def run_ansible(host):
-  res = ssh_exec(host, "cd ansible && ansible-playbook --connection=local --inventory 127.0.0.1, playbook.yml -e letsencrypt_email=something@example.com", False)
-
-  if res.returncode != 0:
-    print_error("fatal error: running ansbible failed")
-
-def prepare_server_and_run_ansible(host):
-  print("\033[95msetting up server {} for ansible\033[0m".format(host))
-
-  print_step("waiting for server to respond to ssh")
-
-  wait_for_ssh(host)
-
-  prepare_known_hosts_file(host)
-
-  res = ssh_exec(host, "true")
-
-  if res.returncode != 0:
-    print_error("fatal error: can not connect server via ssh: {}".format(res.stderr.decode("utf-8")))
-
-    return res.returncode
-
-  print_step("updating server")
-
-  update_server(host)
-
-  print_step("installing ansible")
-
-  install_ansible(host)
-
-  print_step("uploading ansible code")
-
-  upload_ansible_code(host)
-
-  print_step("installing ansible requirements")
-
-  install_ansible_requirements(host)
-
-  print_step("running ansible")
-
-  run_ansible(host)
+    self.run_ansible()
 
 if __name__ == "__main__":
-  prepare_server_and_run_ansible(sys.argv[1])
+  server = Server(sys.argv[1])
+
+  server.prepare_server_and_run_ansible()
