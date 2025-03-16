17755fb495710b176c605c0f87275c2cb04bec3e
Matthias ÃUebler <tx32cv@gmail.com>
2025-03-16 12:00:13 +0100

    add os setups classes

---
+++ os_setups/abstract_os_setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from abc import ABC, abstractmethod
+
+class AbstractOsSetup(ABC):
+  @abstractmethod
+  def update_server(self):
+    pass
+
+  @abstractmethod
+  def install_ansible(self):
+    pass
